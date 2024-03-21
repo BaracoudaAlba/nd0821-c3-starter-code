0dce5b116ecc48c962860a6f5a7fc560889a37cd
Alfredo Baldó Chamorro <alfredo.baldo@alba-robot.com>
2024-03-21 19:55:11 +0100

    data

--- starter/test_main.py
+++ starter/test_main.py
@@ -9,31 +9,51 @@ def test_api_greetings():
     r = client.get("/")
     assert r.status_code == 200
     assert r.json() == "Hello"
-
+# 40,Private,121772,Assoc-voc,11,Married-civ-spouse,Craft-repair,Husband,Asian-Pac-Islander,Male,0,0,40,?,>50K
 def test_post_data_superior():
     data = {
-            'age': 39,
-            'workclass':'State-gov', 
-            'fnlgt':77516,
-            'education':'Bachelors',
-            'education_num':13,
-            'marital_status': 'Never-married',
-            'occupation': 'Adm-clerical',
-            'relationship':'Not-in-family',
-            'race':'White',
+            'age': 40,
+            'workclass':'Private', 
+            'fnlgt':121772,
+            'education':'Assoc-voc',
+            'education_num':11,
+            'marital_status': 'Married-civ-spouse',
+            'occupation': 'Craft-repair',
+            'relationship':'Husband',
+            'race':'Asian-Pac-Islander',
             'sex':'Male',
-            'capital_gain': 2174,
+            'capital_gain': 0,
             'capital_loss': 0,
             'hours_per_week': 40,
-            'native_country':'United-States'
+            'native_country':'?'
             }
     data_example = json.dumps(data)
     r = client.post("/inference", data=data_example)
     assert r.status_code == 200
     assert r.json() == {"predicted": ">50K"}
 
+
+# 32,Private,205019,Assoc-acdm,12,Never-married,Sales,Not-in-family,Black,Male,0,0,50,United-States,<=50K
 def test_post_data_inferior():
-    data_example = json.dumps(Data.model_config["json_schema_extra"]["examples"][1])
+
+    data = {
+            'age': 32,
+            'workclass':'Private', 
+            'fnlgt':205019,
+            'education':'Assoc-acdm',
+            'education_num':12,
+            'marital_status': 'Never-married',
+            'occupation': 'Sales',
+            'relationship':'Not-in-family',
+            'race':'Black',
+            'sex':'Male',
+            'capital_gain': 0,
+            'capital_loss': 0,
+            'hours_per_week': 50,
+            'native_country':'United-States'
+            }
+    
+    data_example = json.dumps(data)
     r = client.post("/inference", data=data_example)
     assert r.status_code == 200
     assert r.json() == {"predicted": "<=50K"}
