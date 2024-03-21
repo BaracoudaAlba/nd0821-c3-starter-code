5ed94a93b36c67ed5de9dcbd79fed954f6686086
Alfredo Baldó Chamorro <alfredo.baldo@alba-robot.com>
2024-03-21 19:59:12 +0100

    data2

--- starter/test_main.py
+++ starter/test_main.py
@@ -36,21 +36,20 @@ def test_post_data_superior():
 # 32,Private,205019,Assoc-acdm,12,Never-married,Sales,Not-in-family,Black,Male,0,0,50,United-States,<=50K
 def test_post_data_inferior():
 
-    data = {
-            'age': 32,
-            'workclass':'Private', 
-            'fnlgt':205019,
-            'education':'Assoc-acdm',
-            'education_num':12,
-            'marital_status': 'Never-married',
-            'occupation': 'Sales',
-            'relationship':'Not-in-family',
-            'race':'Black',
-            'sex':'Male',
-            'capital_gain': 0,
-            'capital_loss': 0,
-            'hours_per_week': 50,
-            'native_country':'United-States'
+    data = {"age": 52,
+            "workclass": "Self-emp-inc",
+            "fnlgt": 287927,
+            "education": "HS-grad",
+            "education_num": 9,
+            "marital_status": "Married-civ-spouse",
+            "occupation": "Exec-managerial",
+            "relationship": "Wife",
+            "race": "White",
+            "sex": "Female",
+            "capital_gain": 15024,
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States"
             }
     
     data_example = json.dumps(data)
